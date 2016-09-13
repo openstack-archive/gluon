133340902caeec7c79d6bc84b0aee246ebab47ee
Thomas Hambleton <thomas.hambleton@nokia.com>
2016-09-13 11:52:06 -0500

    Initial Gluon version
    This commit contains the first semi-working version of the code to use
    the ML2 plugin wrapper class (GluonPlugin).
    
    Previous repositories contained a standalone Gluon service.  This has
    deprecated and its functionality is now in the GluonPlugin class.
    
    Change-Id: Ice8145830ac233caec52f917f0425074badf7a41
    Implements: blueprint gluon-ml2-plugin
    Co-Authored-By: Ian Wells <iawells@cisco.com>
    Co-Authored-By: Tom Hambleton <tomhambleton@gmail.com>
    Co-Authored-By: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

---
+++ gluon/db/sqlalchemy/models.py
@@ -0,0 +1,77 @@
+#    Copyright 2015, Ericsson AB
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import six.moves.urllib.parse as urlparse
+
+from oslo_db.sqlalchemy import models
+from oslo_db import options as db_options
+from oslo_config import cfg
+from sqlalchemy import schema
+from sqlalchemy import (Column, Integer, String)
+from sqlalchemy.ext.declarative import declarative_base
+from gluon.common.particleGenerator import generator as particel_generator
+from gluon.common import paths
+from gluon.sync_etcd.log import logupdate
+from gluon.sync_etcd.log import logdelete
+
+
+sql_opts = [
+    cfg.StrOpt('mysql_engine',
+               default='InnoDB',
+               help='MySQL engine to use.'),
+
+]
+
+# (enikher): for unittests
+_DEFAULT_SQL_CONNECTION = ('sqlite:///' +
+                           paths.state_path_def('gluon.sqlite'))
+
+cfg.CONF.register_opts(sql_opts, 'database')
+db_options.set_defaults(cfg.CONF, _DEFAULT_SQL_CONNECTION, 'gluon.sqlite')
+
+
+class GluonBase(models.TimestampMixin, models.ModelBase):
+
+    @classmethod
+    def get_primary_key_type(cls):
+        return cls._primary_key
+
+    def as_dict(self):
+        d = {}
+        for c in self.__table__.columns:
+            d[c.name] = self[c.name]
+        return d
+
+    @logupdate
+    def save(self, session=None):
+        import gluon.db.sqlalchemy.api as db_api
+
+        if session is None:
+            session = db_api.get_session()
+
+        super(GluonBase, self).save(session)
+
+    @logdelete
+    def delete(self, session=None):
+        import gluon.db.sqlalchemy.api as db_api
+        if session is None:
+            session = db_api.get_session()
+        session.delete(self)
+        session.flush()
+
+
+Base = declarative_base(cls=GluonBase)
+
+particel_generator.build_sql_models(Base)
---
+++ gluon/sync_etcd/thread.py
@@ -0,0 +1,106 @@
+import threading
+import Queue
+import json
+from gluon.common.particleGenerator.generator import getDataBaseGeneratorInstance as getDBGen
+from gluon.db import api as dbapi
+from oslo_log import log as logging
+from oslo_log._i18n import _LE
+from oslo_log._i18n import _LW
+from oslo_log._i18n import _LI
+
+
+import etcd
+
+LOG = logging.getLogger(__name__)
+
+
+class MyData:
+    pass
+
+SyncData = MyData()
+SyncData.sync_thread_running = False
+SyncData.sync_queue = Queue.Queue()
+SyncData.etcd_port = 2379
+SyncData.etcd_host = '127.0.0.1'
+SyncData.source = "proton"
+SyncData.service = "net-l3vpn"
+
+
+class SyncThread(threading.Thread):
+    """ A worker thread that takes takes commands to
+        update etcd with table changes.
+    """
+    def __init__(self, input_q):
+        super(SyncThread, self).__init__()
+        self.input_q = input_q
+        self.db_instance = dbapi.get_instance()
+        self.etcd_client = etcd.Client(host=SyncData.etcd_host, port=SyncData.etcd_port)
+        LOG.info("SyncThread starting")
+
+    def proc_sync_msg(self, msg):
+        try:
+            if msg["operation"] == "update":
+                obj_key = "_".join(msg["key"].split())  # Get rid of spaces
+                etcd_key = "{0:s}/{1:s}/{2:s}/{3:s}".format(SyncData.service, SyncData.source, msg["table"], obj_key)
+                table_class = getDBGen().get_table_class(msg["table"])
+                data = self.db_instance.get_by_primary_key(table_class, msg["key"])
+                values = data.as_dict()
+                d = {}
+                for key in values.iterkeys():
+                    d[key] = str(values[key])
+                json_str = json.dumps(d)
+                self.etcd_client.write(etcd_key, json_str)
+            elif msg["operation"] == "delete":
+                obj_key = "_".join(msg["key"].split())  # Get rid of spaces
+                etcd_key = "{0:s}/{1:s}/{2:s}/{3:s}".format(SyncData.service, SyncData.source, msg["table"], obj_key)
+                self.etcd_client.delete(etcd_key)
+            elif msg["operation"] == "register":
+                obj_key = "_".join(msg["port_id"].split())  # Get rid of spaces
+                port_key = "/gluon/port/{0:s}".format(obj_key)
+                d = {"tenant_id":msg["tenant_id"], "service":msg["service"], "url":msg["url"]}
+                json_str = json.dumps(d)
+                self.etcd_client.write(port_key, json_str)
+            elif msg["operation"] == "deregister":
+                obj_key = "_".join(msg["port_id"].split()) # Get rid of spaces
+                port_key = "/gluon/port/{0:s}".format(obj_key)
+                self.etcd_client.delete(port_key)
+            else:
+                LOG.error(_LE("Unkown operation in msg %s") % (msg["operation"]))
+        except etcd.EtcdKeyNotFound:
+            LOG.warn(_LW("Unknown key %s") % obj_key)
+        except Exception as e:
+            print(e.__doc__)
+            print(e.message)
+            LOG.error(_LE("Error writing to etcd %s, %s") % (e.__doc__, e.message))
+            raise ValueError
+
+    def run(self):
+        while 1:
+            try:
+                msg = self.input_q.get(True, 10.0)
+                LOG.info(_LI("SyncThread: received message %s ") % msg)
+                self.proc_sync_msg(msg)
+            except Queue.Empty:
+                LOG.debug("SyncThread: Queue timeout")
+            except ValueError:
+                LOG.error(_LE("Error processing sync message"))
+                break
+        LOG.error(_LE("SyncThread exiting"))
+        SyncData.sync_thread_running = False
+
+
+def start_sync_thread(**kwargs):
+    """
+    Start the SyncThread.  This should be called in the main function.
+    """
+    if SyncData.sync_thread_running == False:
+        for key, value in kwargs.iteritems():
+            if (key == "service_name"):
+                SyncData.service = value
+            elif (key == "etcd_host"):
+                SyncData.etcd_host = value
+            elif (key == "etcd_port"):
+                SyncData.etcd_port = value
+        SyncData.sync_thread = SyncThread(SyncData.sync_queue)
+        SyncData.sync_thread.start()
+        SyncData.sync_thread_running = True
