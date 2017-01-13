9de1be8f0df61731f42e3712f9de88d37c3a13df
Darla Ahlert <da741q@att.com>
2017-01-13 13:59:16 -0600

    [WIP] Unit testing for db.sqlalchemy.api.py
    Unit tests for db/sqlalchemy/api.py
    
    Change-Id: I28f5d7d997e150e0269cf08e2039265e13c83ec4
    Co-Authored-By: Jin Li <jl7351@att.com>

---
+++ gluon/tests/db/test_api_connection.py
@@ -0,0 +1,55 @@
+#    Copyright 2016, AT&T
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from gluon.db.sqlalchemy import api
+from gluon.tests import base
+from mock import patch
+
+
+class ConnectionTestCase(base.TestCase):
+
+    def setUp(self):
+        super(ConnectionTestCase, self).setUp()
+
+    """
+    test create()
+    """
+    def test_create(self):
+        pass
+
+    """
+    test _add_filters()
+    """
+    def test_add_filters(self):
+        pass
+
+    """
+    test get_list()
+    """
+    def test_get_list(self):
+        pass
+
+    """
+    test get_by_uuid()
+    """
+    def test_get_by_uuid(self):
+        pass
+
+    """
+    test get_by_primary_key()
+    """
+    def test_get_by_primary_key(self):
+        pass
