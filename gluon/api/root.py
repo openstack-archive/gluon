eb8ee44fdb0a8bef79098d8c82375bbb8e8fc82e
JinLi <jl7351@att.com>
2017-04-05 16:52:12 -0700

    Base URI return values
    When the base uri is accessed, returns a list of versions.
    This change mimics what neutron is currently returning when its
    base uri is accessed.
    
    Change-Id: I9115848e9d9b7b7191a85bb83840ee9f5ab3782a

--- gluon/api/root.py
+++ gluon/api/root.py
@@ -22,10 +22,14 @@ import wsmeext.pecan as wsme_pecan
 from gluon.api.baseObject import APIBase
 from gluon.api import link
 from gluon.api.proton_controller import ProtonController
+from gluon.api import types
 
 
 class Version(APIBase):
     """An API version representation."""
+
+    status = types.create_enum_type('CURRENT', 'STABLE', 'DEPRECATED')
+    """Status of the API, which can be CURRENT, STABLE, DEPRECATED"""
 
     id = wtypes.text
     """The ID of the version, also acts as the release number"""
@@ -34,8 +38,9 @@ class Version(APIBase):
     """A Link that point to a specific version of the API"""
 
     @staticmethod
-    def convert(id):
+    def convert(id, status='CURRENT'):
         version = Version()
+        version.status = status
         version.id = id
         version.links = [link.Link.make_link('self', pecan.request.host_url,
                                              id, '', bookmark=True)]
@@ -59,14 +64,7 @@ class Root(APIBase):
     @staticmethod
     def convert(version='v1'):
         root = Root()
-        root.name = "Gluon API"
-        root.description = ("OpenStack Gluon is a port arbiter that maintains "
-                            "a list of ports and bindings of different "
-                            "network backends. A Proton Server is the API "
-                            "server that hosts multiple Protons, i.e. "
-                            "multiple sets of APIs.")
         root.versions = [Version.convert(version)]
-        root.default_version = Version.convert(version)
         return root
 
 
