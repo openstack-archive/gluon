5e67710ee961445328adb49ad00de25f3c3234ca
JinLi <jl7351@att.com>
2017-04-11 17:13:59 -0700

    Moving hardcoded constants to config file
    This is the first commit that starts the process of moving hardcoded
    constants into config filess. There will be more commits in future
    as we identify more hardcoded constants in our code.
    
    Change-Id: I0605567c8fa34cb0e809525b4b9194ebd02751eb

--- gluon/common/paths.py
+++ gluon/common/paths.py
@@ -16,24 +16,12 @@
 #    under the License.
 
 import os
-
 from oslo_config import cfg
 
-PATH_OPTS = [
-    cfg.StrOpt('pybasedir',
-               default=os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                    '../')),
-               help='Directory where gluon python module is installed.'),
-    cfg.StrOpt('bindir',
-               default='$pybasedir/bin',
-               help='Directory where gluon binaries are installed.'),
-    cfg.StrOpt('state_path',
-               default='$pybasedir',
-               help="Top-level directory for maintaining gluon's state."),
-]
+from gluon.conf import config
+
 
 CONF = cfg.CONF
-CONF.register_opts(PATH_OPTS)
 
 
 def basedir_def(*args):
@@ -53,14 +41,14 @@ def state_path_def(*args):
 
 def basedir_rel(*args):
     """Return a path relative to $pybasedir."""
-    return os.path.join(CONF.pybasedir, *args)
+    return os.path.join(CONF.path.pybasedir, *args)
 
 
 def bindir_rel(*args):
     """Return a path relative to $bindir."""
-    return os.path.join(CONF.bindir, *args)
+    return os.path.join(CONF.path.bindir, *args)
 
 
 def state_path_rel(*args):
     """Return a path relative to $state_path."""
-    return os.path.join(CONF.state_path, *args)
+    return os.path.join(CONF.path.state_path, *args)
